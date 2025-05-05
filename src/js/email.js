c716584a2fa768b2e9c3872789945f1f0ee859fe
SlothyKoala <aneethling21@gmail.com>
2025-05-05 14:04:32 +0200

    new file:   .gitignore 	modified:   public/index.html 	new file:   src/js/email.js

---
+++ src/js/email.js
@@ -0,0 +1,52 @@
+import emailjs from '@emailjs/browser';
+
+const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+
+if (publicKey) {
+    emailjs.init({
+        publicKey: publicKey,
+    });
+} else {
+    console.error("EmailJS Public Key is not defined. Make sure your .env file is set up correctly and accessible.");
+}
+
+function sendEmailWithParams(templateParams) {
+    if (!serviceID || !templateID) {
+        console.error("EmailJS Service ID or Template ID is not defined.");
+        return;
+    }
+
+    emailjs.send(serviceID, templateID, templateParams)
+        .then((response) => {
+           console.log('Email sent successfully!', response.status, response.text);
+        }, (error) => {
+           console.log('Failed to send email...', error);
+        });
+}
+function sendEmailFromForm(formElement) {
+     if (!serviceID || !templateID) {
+        console.error("EmailJS Service ID or Template ID is not defined.");
+        return;
+    }
+
+    emailjs.sendForm(serviceID, templateID, formElement)
+        .then((response) => {
+           console.log('Email sent successfully!', response.status, response.text);
+           formElement.reset();
+        }, (error) => {
+           console.log('Failed to send email...', error);
+        });
+}
+const contactForm = document.getElementById('contact-form');
+
+// Add an event listener for form submission
+if (contactForm) {
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        sendEmailFromForm(this);
+    });
+} else {
+    console.warn("Form with ID 'contact-form' not found. Email sending functionality may not work.");
+}
